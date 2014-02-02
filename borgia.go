b38fdbea4870586db69b149b3ca30121fa65c6e2
James Koppen <jk@jameskoppen.com>
2014-02-01 23:56:18 -0600

    Ran go fmt

--- borgia.go
+++ borgia.go
@@ -46,19 +46,19 @@ flags on the command-line.
 package main
 
 import (
-  "bufio"
-  "flag"
-  "fmt"
-  "io"
-  "io/ioutil"
-  "net/http"
-  "net/url"
-  "math/rand"
-  "os"
-  "strings"
-  "time"
-  "github.com/kurrik/twittergo"
-  "github.com/kurrik/oauth1a"
+	"bufio"
+	"flag"
+	"fmt"
+	"github.com/kurrik/oauth1a"
+	"github.com/kurrik/twittergo"
+	"io"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"time"
 )
 
 // Prefix is a Markov chain prefix of one or more words.
@@ -66,129 +66,129 @@ type Prefix []string
 
 // String returns the Prefix as a string (for use as a map key).
 func (p Prefix) String() string {
-  return strings.Join(p, " ")
+	return strings.Join(p, " ")
 }
 
 // Shift removes the first word from the Prefix and appends the given word.
 func (p Prefix) Shift(word string) {
-  copy(p, p[1:])
-  p[len(p)-1] = word
+	copy(p, p[1:])
+	p[len(p)-1] = word
 }
 
 // Chain contains a map ("chain") of prefixes to a list of suffixes.
 // A prefix is a string of prefixLen words joined with spaces.
 // A suffix is a single word. A prefix can have multiple suffixes.
 type Chain struct {
-  chain     map[string][]string
-  prefixLen int
+	chain     map[string][]string
+	prefixLen int
 }
 
 // NewChain returns a new Chain with prefixes of prefixLen words.
 func NewChain(prefixLen int) *Chain {
-  return &Chain{make(map[string][]string), prefixLen}
+	return &Chain{make(map[string][]string), prefixLen}
 }
 
 // Build reads text from the provided Reader and
 // parses it into prefixes and suffixes that are stored in Chain.
 func (c *Chain) Build(r io.Reader) {
-  br := bufio.NewReader(r)
-  p := make(Prefix, c.prefixLen)
-  for {
-    var s string
-    if _, err := fmt.Fscan(br, &s); err != nil {
-      break
-    }
-    key := p.String()
-    c.chain[key] = append(c.chain[key], s)
-    p.Shift(s)
-  }
+	br := bufio.NewReader(r)
+	p := make(Prefix, c.prefixLen)
+	for {
+		var s string
+		if _, err := fmt.Fscan(br, &s); err != nil {
+			break
+		}
+		key := p.String()
+		c.chain[key] = append(c.chain[key], s)
+		p.Shift(s)
+	}
 }
 
 // Generate returns a string of at most n words generated from Chain.
 func (c *Chain) Generate(n int) string {
-  p := make(Prefix, c.prefixLen)
-  var words []string
-  for i := 0; i < n; i++ {
-    choices := c.chain[p.String()]
-    if len(choices) == 0 {
-      break
-    }
-    next := choices[rand.Intn(len(choices))]
-    words = append(words, next)
-    p.Shift(next)
-  }
-  return strings.Join(words, " ")
+	p := make(Prefix, c.prefixLen)
+	var words []string
+	for i := 0; i < n; i++ {
+		choices := c.chain[p.String()]
+		if len(choices) == 0 {
+			break
+		}
+		next := choices[rand.Intn(len(choices))]
+		words = append(words, next)
+		p.Shift(next)
+	}
+	return strings.Join(words, " ")
 }
 
 func LoadCredentials() (client *twittergo.Client, err error) {
-  credentials, err := ioutil.ReadFile("CREDS")
-  if err != nil {
-    return
-  }
-  lines := strings.Split(string(credentials), "\n")
-  config := &oauth1a.ClientConfig{
-    ConsumerKey: lines[0],
-    ConsumerSecret: lines[1],
-  }
-  user := oauth1a.NewAuthorizedConfig(lines[2], lines[3])
-  client = twittergo.NewClient(config, user)
-  return
+	credentials, err := ioutil.ReadFile("CREDS")
+	if err != nil {
+		return
+	}
+	lines := strings.Split(string(credentials), "\n")
+	config := &oauth1a.ClientConfig{
+		ConsumerKey:    lines[0],
+		ConsumerSecret: lines[1],
+	}
+	user := oauth1a.NewAuthorizedConfig(lines[2], lines[3])
+	client = twittergo.NewClient(config, user)
+	return
 }
 
 func main() {
-  // Register command-line flags.
-  numWords := flag.Int("words", 18, "maximum number of words to print")
-  prefixLen := flag.Int("prefix", 2, "prefix length in words")
+	// Register command-line flags.
+	numWords := flag.Int("words", 18, "maximum number of words to print")
+	prefixLen := flag.Int("prefix", 2, "prefix length in words")
 
-  flag.Parse()                     // Parse command-line flags.
-  rand.Seed(time.Now().UnixNano()) // Seed the random number generator.
+	flag.Parse()                     // Parse command-line flags.
+	rand.Seed(time.Now().UnixNano()) // Seed the random number generator.
 
-  c := NewChain(*prefixLen)     // Initialize a new Chain.
-  c.Build(os.Stdin)             // Build chains from standard input.
-  text := c.Generate(*numWords) // Generate text.
+	c := NewChain(*prefixLen)     // Initialize a new Chain.
+	c.Build(os.Stdin)             // Build chains from standard input.
+	text := c.Generate(*numWords) // Generate text.
 
-  var (
-    err error
-    client *twittergo.Client
-    req *http.Request
-    resp *twittergo.APIResponse
-    tweet *twittergo.Tweet
-  )
-  client, err = LoadCredentials()
-  if err != nil {
-    fmt.Printf("Could not parse CREDENTIALS file: %v\n", err)
-    os.Exit(1)
-  }
-  data := url.Values{}
-  data.Set("status", text)
-  body := strings.NewReader(data.Encode())
-  req, err = http.NewRequest("POST", "/1.1/statuses/update.json", body)
-  if err != nil {
-    fmt.Printf("Could not parse request: %v\n", err)
-    os.Exit(1)
-  }
-  req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-  resp, err = client.SendRequest(req)
-  if err != nil {
-    fmt.Printf("Could not send request: %v\n", err)
-    os.Exit(1)
-  }
-  tweet = &twittergo.Tweet{}
-  err = resp.Parse(tweet)
-  if err != nil {
-    if rle, ok := err.(twittergo.RateLimitError); ok {
-      fmt.Printf("Rate limited, reset at %v\n", rle.Reset)
-    } else if errs, ok := err.(twittergo.Errors); ok {
-      for i, val := range errs.Errors() {
-        fmt.Printf("Error #%v - ", i + 1)
-        fmt.Printf("Code: %v ", val.Code())
-        fmt.Printf("Msg: %v\n", val.Message())
-      }
-    } else {
-      fmt.Printf("Problem parsing response: %v\n", err)
-    }
-    os.Exit(1)
-  }
-  fmt.Printf("ID: %v\n", tweet.Id())
-  fmt.Printf("Tweet: %v\n", tweet.Text())
+	var (
+		err    error
+		client *twittergo.Client
+		req    *http.Request
+		resp   *twittergo.APIResponse
+		tweet  *twittergo.Tweet
+	)
+	client, err = LoadCredentials()
+	if err != nil {
+		fmt.Printf("Could not parse CREDENTIALS file: %v\n", err)
+		os.Exit(1)
+	}
+	data := url.Values{}
+	data.Set("status", text)
+	body := strings.NewReader(data.Encode())
+	req, err = http.NewRequest("POST", "/1.1/statuses/update.json", body)
+	if err != nil {
+		fmt.Printf("Could not parse request: %v\n", err)
+		os.Exit(1)
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	resp, err = client.SendRequest(req)
+	if err != nil {
+		fmt.Printf("Could not send request: %v\n", err)
+		os.Exit(1)
+	}
+	tweet = &twittergo.Tweet{}
+	err = resp.Parse(tweet)
+	if err != nil {
+		if rle, ok := err.(twittergo.RateLimitError); ok {
+			fmt.Printf("Rate limited, reset at %v\n", rle.Reset)
+		} else if errs, ok := err.(twittergo.Errors); ok {
+			for i, val := range errs.Errors() {
+				fmt.Printf("Error #%v - ", i+1)
+				fmt.Printf("Code: %v ", val.Code())
+				fmt.Printf("Msg: %v\n", val.Message())
+			}
+		} else {
+			fmt.Printf("Problem parsing response: %v\n", err)
+		}
+		os.Exit(1)
+	}
+	fmt.Printf("ID: %v\n", tweet.Id())
+	fmt.Printf("Tweet: %v\n", tweet.Text())
 }
